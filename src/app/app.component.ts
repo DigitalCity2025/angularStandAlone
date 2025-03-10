95fa26e969c4ffa4e8a8e0286794882655ffe199
Khun <lykhun@gmail.com>
2025-03-10 11:07:27 +0100

    effect + commputed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, inject, Injector } from '@angular/core';
+import { Component, computed, effect, Inject, inject, Injector, Signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { routes } from './app.routes';
 import { NavRoutes } from './types/nav-routes';
@@ -20,12 +20,23 @@ export class AppComponent {
   ressource = inject(Injector).get('ressource1');
   listCourseService = inject(ListCourseService);
 
+  // count = 0;
+  count: Signal<number>
+
   constructor(
     // @Inject('ressource1') ressource: number,
     // injection par constructeur
     // autreRessource: Ressource2
   ) {
     console.log(this.ressource);
+
+    // effect(() => {
+    //   this.count = this.listCourseService.list().length;
+    // })
+
+    this.count 
+      = computed(() => this.listCourseService.list().length)
+
     ///this.autreRessource.method();
   }
 }
