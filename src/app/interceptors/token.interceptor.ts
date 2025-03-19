1115daeb0b34812b3ed9b79784a00a841e108980
Khun <lykhun@gmail.com>
2025-03-19 11:30:03 +0100

    refresh token

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,14 +1,45 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpClient, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { SessionService } from '../services/session.service';
+import { catchError, from, mergeMap, of, tap } from 'rxjs';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  
   const sessionService = inject(SessionService);
-  if(!sessionService.session().token) {
+  
+  let token = sessionService.session().token
+  if(!token) {
     return next(req);
   }
-  const clone = req.clone({ setHeaders: {
-    Authorization: 'Bearer ' + sessionService.session().token
-  } });
-  return next(clone);
+
+  // si mon token a expiré
+  if(sessionService.session().exp < new Date()) {
+    // from transformer une promise en observable
+    return from(fetch('http://localhost:5166/api/refreshToken?token=' + token)
+      .then(res => res.json())
+    ).pipe(
+      // side effect (opération supplémnetaire)
+      tap(({token}) => sessionService.start(token)),
+      // capturer une erreur de la requète de rafraichissement
+      catchError(() => {
+        sessionService.clear();
+        return of()
+      }),
+      // effectuer une requete supplementaire
+      mergeMap(({token}) => {
+        const clone = req.clone({ setHeaders: {
+          Authorization: 'Bearer ' + token
+        } });
+        return next(clone);
+      })
+    )
+  }
+  else {
+    const clone = req.clone({ setHeaders: {
+      Authorization: 'Bearer ' + sessionService.session().token
+    } });
+    return next(clone);
+  }
+
+
 };
